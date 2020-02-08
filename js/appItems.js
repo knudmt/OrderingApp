b84ff4cf39c0b3b560b8d4587cd4431beb213332
knudmt <knudmt@outlook.com>
2020-02-08 16:28:20 -0500

    updated classes, reduced code, added swift order

--- js/appItems.js
+++ js/appItems.js
@@ -1,36 +1,8 @@
-class AppItems 
+class Items 
 {
     constructor(description, price, quanity){
-        this._description = description;
-        this._price = price;
-        this._quanity = quanity;
-    }
-
-    get description(){
-        return this._description;
-    }
-
-    set description(newDescription){
-        this._description = newDescription;
-    }
-
-    get price(){
-        return this._price;
-    }
-
-    set price(newPrice){
-        this._price = newPrice;
-    }
-
-    get quanity(){
-        return this._quanity;
-    }
-
-    set quanity(newQuanity){
-        this._quanity = newQuanity;
-    }
-
-    log(){
-        console.log(this._description + ', ' + 'at a price of: ' + this._price + ' with a quanity of: ' + this._quanity);
+        this.description = description;
+        this.price = price;
+        this.quanity = quanity;
     }
 }
